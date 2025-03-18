381da652eec7700cda205608afbbddb5a656780f
woohs0930 <woohs0930@gmail.com>
2025-03-18 17:47:42 +0900

    2025-03-18 hswoo
    * Spring Batch
    -> 배치 job 관련 개념 및 기초 코드 작성

--- spring-batch/src/main/java/io/github/loki/batch/springbatch/DBJobConfiguration.java
+++ spring-batch/src/main/java/io/github/loki/batch/springbatch/DBJobConfiguration.java
@@ -12,63 +12,44 @@
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //
-//@RequiredArgsConstructor
 //@Configuration
-//public class HelloJobConfiguration {
+//@RequiredArgsConstructor
+//public class DBJobConfiguration {
 //
 //    private final JobBuilderFactory jobBuilderFactory;
 //    private final StepBuilderFactory stepBuilderFactory;
 //
-//
 //    @Bean
-//    public Job helloJob() {
-//
-//        /**
-//         * Job 생성
-//         *  기본적으로 "step"을 필수로 갖고있어야 함
-//         * */
-//        return jobBuilderFactory.get("helloJob")
-//                .start(helloStep1())
-//                .next(helloStep2())
+//    public Job job() {
+//        return jobBuilderFactory.get("job")
+//                .start(step1())
+//                .next(step2())
 //                .build();
 //    }
 //
 //    @Bean
-//    public Step helloStep1() {
-//
-//        /**
-//         * Step 에서는 Tasklet을 무한 반복 함.
-//         * */
-//        return stepBuilderFactory.get("helloStep1")
+//    public Step step1() {
+//        return stepBuilderFactory.get("step1")
 //                .tasklet(new Tasklet() {
-//                    // 익명클래스 생성
 //                    @Override
 //                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-//                        System.out.println(" =========================");
-//                        System.out.println(" >> Hello Spring Batch!!");
-//                        System.out.println(" =========================");
-//                        return RepeatStatus.FINISHED; // null 과 같은 개념
+//                        System.out.println("Step1 was executed");
+//                        return RepeatStatus.FINISHED; // FINISH를 안주면 기본적으로 무한 반복
 //                    }
 //                })
 //                .build();
 //    }
 //
 //    @Bean
-//    public Step helloStep2() {
-//
-//        return stepBuilderFactory.get("helloStep2")
+//    public Step step2() {
+//        return stepBuilderFactory.get("step2")
 //                .tasklet(new Tasklet() {
-//                    // 익명클래스 생성
 //                    @Override
 //                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-//                        System.out.println(" =========================");
-//                        System.out.println(" >> Step2 was executed!!");
-//                        System.out.println(" =========================");
-//                        return RepeatStatus.FINISHED; // null 과 같은 개념
+//                        System.out.println("Step2 was executed");
+//                        return RepeatStatus.FINISHED;
 //                    }
 //                })
 //                .build();
 //    }
-//
-//
 //}
