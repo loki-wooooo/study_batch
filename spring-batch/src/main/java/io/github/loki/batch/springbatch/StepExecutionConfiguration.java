40f21426910593d376704ae613fbd39a473ea79e
woohs0930 <woohs0930@gmail.com>
2025-04-03 17:25:28 +0900

    2025-04-03 hswoo
    *Spring Batch
    -> Step execution 주석 처리

--- spring-batch/src/main/java/io/github/loki/batch/springbatch/StepExecutionConfiguration.java
+++ spring-batch/src/main/java/io/github/loki/batch/springbatch/StepExecutionConfiguration.java
@@ -1,61 +1,61 @@
-package io.github.loki.batch.springbatch;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RequiredArgsConstructor
-public class StepExecutionConfiguration {
-
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Job batchJob() {
-        return this.jobBuilderFactory
-                .get("job")
-                .start(step1())
-                .next(step2())
-                .next(step3())
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory
-                .get("step1")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("step1 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory
-                .get("step2")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("step2 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory
-                .get("step3")
-                .tasklet((stepContribution, chunkContext) -> {
-                    System.out.println("step3 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-}
+//package io.github.loki.batch.springbatch;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class StepExecutionConfiguration {
+//
+//    private final JobBuilderFactory jobBuilderFactory;
+//    private final StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Job batchJob() {
+//        return this.jobBuilderFactory
+//                .get("job")
+//                .start(step1())
+//                .next(step2())
+//                .next(step3())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return stepBuilderFactory
+//                .get("step1")
+//                .tasklet((stepContribution, chunkContext) -> {
+//                    System.out.println("step1 has executed");
+//                    return RepeatStatus.FINISHED;
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return stepBuilderFactory
+//                .get("step2")
+//                .tasklet((stepContribution, chunkContext) -> {
+//                    System.out.println("step2 has executed");
+//                    return RepeatStatus.FINISHED;
+//                })
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step3() {
+//        return stepBuilderFactory
+//                .get("step3")
+//                .tasklet((stepContribution, chunkContext) -> {
+//                    System.out.println("step3 has executed");
+//                    return RepeatStatus.FINISHED;
+//                })
+//                .build();
+//    }
+//}
